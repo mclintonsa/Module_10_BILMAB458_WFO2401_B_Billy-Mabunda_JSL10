c1cca02c4244b430e90dc9f59c1c27cf97d5ccec
Billy Clinton Mabunda <mclintonsa@gmail.com>
2024-04-18 19:28:35 +0200

    Adjusted the findMostRecentBook function to correctly compare dates and find the most recent book

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -34,9 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function findMostRecentBook(books) {
-    // 🪲 Bug: Logic error
-    return books.reduce((mostRecent, book) => new Date(book.published) < new Date(mostRecent.published) ? book : mostRecent);
-}
+    return books.reduce((mostRecent, book) => {
+      const mostRecentDate = new Date(mostRecent.published);
+      const bookDate = new Date(book.published);
+      return bookDate > mostRecentDate ? book : mostRecent;
+    });
+  }
 
 function findIntersection(setA, setB) {
     // 🪲 Bug: Incorrect logic
